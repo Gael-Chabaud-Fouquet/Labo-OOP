7fbdb9c4ea9d9ae2748b7e0d14c7f786a84e2c2e
Gael-Chabaud-Fouquet <2430057@bart.ca>
2025-04-25 19:38:51 -0400

    Finished the DVD-like square lab

--- raylibbetter/src/Labo3_DVD.cpp
+++ raylibbetter/src/Labo3_DVD.cpp
@@ -1,46 +1,36 @@
 #include "Labo3_DVDEntrypoint.h"
 #include <raylib.h>
 
-//color guide: https://c20.reclaimers.net/h1/engine/hard-coded-data/
-//je m'arrete a "Multiplayer armor colors"
+DVDCube::DVDCube(int startX, int startY)
+    : PosX(startX), PosY(startY), Width(25), Height(25), VelocityX(400), VelocityY(150), DVDColor(Color{255, 86, 185, 255}) {}
 
-DVDCube dvd;
-
-void DVDCube::Draw(){
-    int PosX;
-    int PosY;
-    int Width = 10;
-    int Height = 10;
-    Color DVDColor = Color{255, 86, 185, 255};
+void DVDCube::Draw() {
     DrawRectangle(PosX, PosY, Width, Height, DVDColor);
 }
-void DVDCube::Move(){
-    int VelocityX = 30;
-    int VelocityY = 10;
-    
-    
-    PosX +=VelocityX;// * GetFrameTime();
-    PosY += VelocityY;// * GetFrameTime();
 
+void DVDCube::Move() {
+    PosX += VelocityX * GetFrameTime();
+    PosY += VelocityY * GetFrameTime();
 
-
-if (PosY > 300 || PosY < 0) {
-    VelocityY *= -1;
-}
-if (PosX > 400 || PosX < 0) {
-    VelocityX *= -1;
-}
+    if (PosY + Height > GetScreenHeight() || PosY < 0) {
+        VelocityY *= -1;
+    }
+    if (PosX + Width > GetScreenWidth() || PosX < 0) {
+        VelocityX *= -1;
+    }
 }
 
-int main(void){
+int main(void) {
     Color BackgroundColor = {255, 255, 255, 255};
 
-    const int ScreenWidth = 400;
-    const int ScreenHeight = 300;
-    int FrameRate = 100;
+    const int ScreenWidth = 1000;
+    const int ScreenHeight = 600;
+    int FrameRate = 120;
 
     InitWindow(ScreenWidth, ScreenHeight, "DVD Cube");
     SetTargetFPS(FrameRate);
+
+    DVDCube dvd(50, 50);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -50,4 +40,5 @@ int main(void){
         EndDrawing();
     }
     CloseWindow();
+    return 0;
 }
--- raylibbetter/src/Labo3_DVDEntrypoint.h
+++ raylibbetter/src/Labo3_DVDEntrypoint.h
@@ -1,15 +1,31 @@
 #pragma once
 #include <raylib.h>
 
-class DVDCube
-{
-    private:
-    int PosX;
-    int PosY;
-    int Lenght;
-    int Width;
+// class DVDCube
+// {
+//     private:
+//     int PosX;
+//     int PosY;
+//     int Lenght;
+//     int Width;
 
-    public:
-    void Draw();
-    void Move();
-};
+//     public:
+//     DVDCube(int startX, int startY) {int PosX(startX); int PosY(startY);}
+//     void Draw();
+//     void Move();
+// };
+
+class DVDCube {
+    private:
+        int PosX;
+        int PosY;
+        int Width;
+        int Height;
+        int VelocityX;
+        int VelocityY;
+        Color DVDColor;
+        public:
+        DVDCube(int StartX, int StartY);
+        void Move();
+        void Draw();
+    };
